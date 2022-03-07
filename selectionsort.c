9d40ffabf343998892c7afbdb40b9c2450647183
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-03-07 19:07:33 +0530

    selection sort

---
+++ selectionsort.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+void insert(int a[],int n);
+void sort(int a[],int n);
+void display(int a[],int n);
+int main()
+{
+    int n;
+    printf("Enter the Number of elements");
+    scanf("%d",&n);
+    int a[n];
+    insert(a,n);
+    sort(a,n);
+    display(a,n);
+    return 0;
+}
+void insert(int a[],int n){
+    printf("enter the elements");
+    for(int i= 0 ; i<n;i++){
+        scanf("%d",&a[i]);
+    }
+}
+void display(int a[],int n){
+    for(int i=0; i<n;i++){
+        printf("%d\t",a[i]);
+    }
+}
+
+void sort(int a[], int n){
+    int i,j,temp, pos, min;
+    for(i=0; i<n; i++){
+        min = a[i];
+        pos = i;
+        for(j= i+1; j<n; j++){
+            if(min>a[j]){
+                min = a[j];
+                pos = j;
+            }
+        }
+        temp = a[i];
+        a[i] = a[pos];
+        a[pos] = temp;
+    }
+}
