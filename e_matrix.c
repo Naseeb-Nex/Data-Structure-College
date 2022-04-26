26020a010c9f6f6cd3910af59216146c0c4156db
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-04-26 12:00:31 +0530

    Working on it

---
+++ e_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void input(){
+
+}
+
+void display(){
+
+}
+
+void tuple(){
+
+}
+
+void trans(){
+
+}
+
+void add(){
+
+}
+
+void main(){
+    int a[10][10], b[10][10], t1[10][10], t2[10][10], tr1[10][10], tr2[10][10], s[10][10];
+    int r1, c1, r2, c2, k, l;
+
+    printf("First matrix ");
+
+}
