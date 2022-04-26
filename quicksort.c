3d7d36736bb603587b38f3e86bc3a84e56d20358
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-04-26 19:44:00 +0530

    Quicksort

--- quicksort.c
+++ quicksort.c
@@ -3,6 +3,7 @@
 void insertion(int[], int);
 void display(int[], int);
 void qsort(int[], int, int);
+int partition(int[], int, int);
 
 int main(){
     int n;
@@ -26,13 +27,43 @@ void insertion(int arr[], int l){
     }
 }
 
+
+int partition(int a[], int low, int high){
+    int i = low;
+    int j = high;
+    int x = a[low];
+    while (i<=j)
+    {
+        while (i<high && a[i]<=x)
+        {
+            i++;
+        }
+        while (j>=0 && a[j]>x)
+        {
+            j--;
+        }
+        if(i<j){
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        
+        int temp = a[low];
+        a[low] = a[j];
+        a[j] = temp;
+        return j;
+    }
+    
+}
+
 void qsort(int arr[], int low,int high){
     if(low<high){
-        int mid = partition(arr, low, high)
+        int mid = partition(arr, low, high);
+        qsort(arr, low, mid -1);
+        qsort(arr, mid+1, high);
     }
-
-     
 }
+
 
 void display(int arr[], int l){
     int i;
